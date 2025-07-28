bd7281e6809d319ff97bc41fb259342c6cc3792b
Joagai23 <jaguirregomezcorta@gmail.com>
2025-07-28 10:18:00 +0200

    Remove base-stonefish c++ dependencies

--- src/stonefish_silver/include/SilverManager.h
+++
@@ -1,8 +0,0 @@
-#include <Stonefish/core/SimulationManager.h>
-
-class SilverManager : public sf::SimulationManager
-{
-public:
-    SilverManager(sf::Scalar stepsPerSecond);
-    void BuildScenario();
-};
